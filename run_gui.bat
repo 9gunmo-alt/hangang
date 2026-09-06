d5b1ddd59d120f7d7520d4335521e108b7f93c4d
9gunmo-alt <9gunmo@gmail.com>
2026-09-06 16:01:10 +0900

    봇 수정

--- run_gui.bat
+++ run_gui.bat
@@ -1,3 +1,8 @@
 @echo off
 cd /d %~dp0
-start "" pythonw scraper\app_gui.py
+REM VBS trick: run python (not pythonw) but hide the console window
+echo Set s=CreateObject("WScript.Shell") > "%TEMP%\hanriver_run.vbs"
+echo s.CurrentDirectory = "%~dp0" >> "%TEMP%\hanriver_run.vbs"
+echo s.Run "python scraper\app_gui.py", 0, False >> "%TEMP%\hanriver_run.vbs"
+cscript //nologo "%TEMP%\hanriver_run.vbs"
+del "%TEMP%\hanriver_run.vbs"
