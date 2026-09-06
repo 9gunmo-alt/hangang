2468b8282619fbc303f1c63d30ba37e29077b718
9gunmo-alt <9gunmo@gmail.com>
2026-09-06 17:24:30 +0900

    수정

--- run_gui.bat
+++ run_gui.bat
@@ -1,8 +1,8 @@
 @echo off
-cd /d %~dp0
-REM VBS trick: run python (not pythonw) but hide the console window
-echo Set s=CreateObject("WScript.Shell") > "%TEMP%\hanriver_run.vbs"
-echo s.CurrentDirectory = "%~dp0" >> "%TEMP%\hanriver_run.vbs"
-echo s.Run "python scraper\app_gui.py", 0, False >> "%TEMP%\hanriver_run.vbs"
-cscript //nologo "%TEMP%\hanriver_run.vbs"
-del "%TEMP%\hanriver_run.vbs"
+cd /d "%~dp0"
+echo [run_gui] folder: %CD%
+echo [run_gui] starting bot...
+python scraper\app_gui.py
+echo.
+echo [run_gui] python exited. (code %errorlevel%)
+pause
